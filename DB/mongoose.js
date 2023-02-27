145dd26fbe0e00cb9a3f9f514b3a355c81c9c9a6
Pooja Baba <babapooja6@gmail.com>
2023-02-27 11:39:25 -0500

    updated connection string

--- DB/mongoose.js
+++ DB/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 
 // var DB = 'mongodb://localhost:27017/TaskManager0;'
 var DB =
-  "mongodb://task-manager-db:T63akiE3YJ7kKxQamPXC2YpclmQY01TugqK7QZV6pZrhkZMIBfPdC757dcR0jKOQZUDi8kzGZicCACDbQzHvmg%3D%3D@task-manager-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@task-manager-db@/TaskManager";
+  "mongodb://task-manager-db:T63akiE3YJ7kKxQamPXC2YpclmQY01TugqK7QZV6pZrhkZMIBfPdC757dcR0jKOQZUDi8kzGZicCACDbQzHvmg%3D%3D@task-manager-db.mongo.cosmos.azure.com:10255/?ssl=true&retrywrites=false&maxIdleTimeMS=120000&appName=@task-manager-db@TaskManager";
 mongoose.Promise = global.Promise;
 mongoose
   .connect(DB, { useNewUrlParser: true })
